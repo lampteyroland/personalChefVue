11f9866de4db73f0b610a1aada36162abbebbc63
roland <lampteyroland8@gmail.com>
2023-09-20 06:37:41 +0000

    boostrap setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path' // Import path module
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    },
+  },
+  server: {
+    port: 3000,
+    open: true, 
+  },
+  build: {
+    outDir: 'dist', 
+  },
 })
